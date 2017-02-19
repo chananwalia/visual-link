d4d2d472c82427eeafc8814ade2137b6aa782dd2
Ryan Hoque <ryanhoque2@gmail.com>
2017-02-18 23:21:53 -0800

    preliminary setter

--- backend/setter.php
+++ backend/setter.php
@@ -17,20 +17,60 @@
 		// enter new combo into db
 
 		// generate a unique combo for the link
+
+		// initialize possibilities
 		$colors = array("pink", "red", "orange", "yellow", "green", "blue", "purple", "teal", "maroon");
 		$animals = array("elephant", "penguin", "turtle", "pig", "dog", "cat", "unicorn", "monkey", "whale");
 		$bgs = array("beach", "jungle", "city", "snow", "space", "desert", "mountain", "garden", "underwater");
 
+		// get all combos in the db
 		$now = time();
+		$db_combos = array();
 		$query = "SELECT * FROM thdb WHERE from_unixtime({$now}) < expire-time";
 		$response = @mysqli_query($dbc, $query);
-		
+		while ($row = mysqli_fetch_array($response)) {
+			$db_combos[] = $row['color'] . "_" . $row['animal'] . "_" . $row['bg'];
+		}
+
+		// generate all possible combos
+		$all_combos = array();
+		foreach ($colors as $c) {
+			foreach ($animals as $a) {
+				foreach ($bgs as $b) {
+					$all_combos[] = $c . "_" $a . "_" $b;
+				}
+			}
+		}
+
+		sort($db_combos);
+		sort($all_combos);
+
+		// loop through all combos, making new array of unused combos
+		$size1 = count($all_combos);
+		$size2 = count($db_combos);
+		$x = 0;
+		$y = 0;
+		$new_combos = array();
+		while ($x < $size1 && $y < $size2) {
+			if ($all_combos[$x] != $db_combos[$y]) {
+				$new_combos[] = $all_combos[$x];
+				$x++;
+			} else {
+				$x++;
+				$y++;
+			}
+		}
+
+		//generate random combo
+		$r = rand(0, count($new_combos) - 1);
+		$fields = explode("_", $new_combos[$r]);
+
 
 		$expiration = date('Y-m-d H:i:s', $now + 259200); // default is to expire in 3 days.
 		$query = "INSERT INTO thdb (link, color, animal, bg, expire-time) 
 			VALUES (?, ?, ?, ?, ?)";
 		$stmt = mysqli_prepare($dbc, $query);
-		mysqli_stmt_bind_param($stmt, "sssss", $link, $color, $animal, $bg, $expiration);
+		mysqli_stmt_bind_param($stmt, "sssss", $link, $fields[0], $fields[1], $fields[2], $expiration);
 		mysqli_stmt_execute($stmt);
 		mysqli_stmt_close($stmt);
 		mysqli_close($dbc);
