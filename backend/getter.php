a3ae636ebe1920a8a44f381e61641c7f22b589ba
Ryan Hoque <ryanhoque2@gmail.com>
2017-02-18 18:28:49 -0800

    getter prelimiary implementation

--- backend/getter.php
+++ backend/getter.php
@@ -9,5 +9,19 @@
 	$bg = $_GET["bg"];
 
 	$query = "SELECT link FROM thdb WHERE color = {$color} AND animal = {$animal} AND bg = {$bg} ORDER BY -id LIMIT 1";
+	$response = @mysqli_query($dbc, $query);
+
+	$currtime = time();
+
+	if ($response) {
+		while ($row = mysqli_fetch_array($response)) {
+			if ($currtime > $row['expire-time']) {
+				die("No link for this combo!");
+			}
+			print json_encode(array("link"=>$row['link']));
+		}
+	} else {
+		die("No link for this combo!");
+	}
 
 ?>
