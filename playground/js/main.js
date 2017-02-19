58d33c83deda8c014d5cfedd1d8d4b21d8cfdc72
Chanan Walia <chananwalia@gmail.com>
2017-02-19 00:49:55 -0800

    js updates

--- playground/js/main.js
+++ playground/js/main.js
@@ -12,19 +12,52 @@ $(document).ready(function() {
     // $("#selections").hide();
 
     for (var i = 0; i < animals.length; i++) {
-    	$("#selections").append('<div class="col-md-4"><a href="#" class="animal-selection"><div class="selection-button" id="' + animals[i] + '"><img class="selection-image" src="http://vignette3.wikia.nocookie.net/clubpenguin/images/a/a9/Penguin_Player_card_look_1222333.png/revision/latest?cb=20140723221416" height="150px"></div></a></div>');
+    	$("#selection-group").append('<div class="col-md-4"><a href="#" class="animal-selection" id="' + animals[i] + '"><div class="selection-button">' + animals[i] + '<img class="selection-image" src="http://vignette3.wikia.nocookie.net/clubpenguin/images/a/a9/Penguin_Player_card_look_1222333.png/revision/latest?cb=20140723221416" height="150px"></div></a></div>');
     }
 
-    $(".animal-selection").click(function(event) {
-    	var selected_animal = event.target.id;
+    $("#selection-group").on("click", ".animal-selection", function() {
+    	console.log("animal selected");
+    	selected_animal = $(this).closest("a").prop("id");
 		console.log(selected_animal);
     	$(".sub-menu").removeClass("active");
     	$("#color-tab").addClass("active");
-    	$("#selections").hide();
-    	$("#selections").show();
+    	$("#selection-group").empty();
+    	for (var i = 0; i < colors.length; i++) {
+    		$("#selection-group").append('<div class="col-md-4"><a href="#" class="color-selection" id="' + colors[i] + '"><div class="selection-button">' + colors[i] + '<img class="selection-image" src="http://vignette3.wikia.nocookie.net/clubpenguin/images/a/a9/Penguin_Player_card_look_1222333.png/revision/latest?cb=20140723221416" height="150px"></div></a></div>');	
+    	}
     });
 
-    $("#selection-group").append()
+    $("#selection-group").on("click", ".color-selection", function() {
+    	console.log("color selected");
+    	selected_color = $(this).closest("a").prop("id");
+		console.log(selected_color);
+    	$(".sub-menu").removeClass("active");
+    	$("#bg-tab").addClass("active");
+    	$("#selection-group").empty();
+    	for (var i = 0; i < bgs.length; i++) {
+    		$("#selection-group").append('<div class="col-md-4"><a href="#" class="bg-selection" id="' + bgs[i] + '"><div class="selection-button">' + bgs[i] + '<img class="selection-image" src="http://vignette3.wikia.nocookie.net/clubpenguin/images/a/a9/Penguin_Player_card_look_1222333.png/revision/latest?cb=20140723221416" height="150px"></div></a></div>');	
+    	}
+    });
+
+    $("#selection-group").on("click", ".bg-selection", function() {
+    	console.log("bg selected");
+    	selected_bg = $(this).closest("a").prop("id");
+		console.log(selected_bg);
+		$.ajax({
+    	  url: "http://th.chisaz.com/backend/getter.php",
+    	  type: "get",
+    	  data: {
+    	  	bg: selected_bg,
+    	  	color: selected_color,
+    	  	animal: selected_animal
+    	  },
+    	  dataType: "text",
+    	  success: function(data) {
+    	    var json = $.parseJSON(data);
+    	    window.location.replace(json.link);
+    	  }
+    	});
+    });
 
     $("#create-button").click(function() {
     	$("#selections").hide();
@@ -41,13 +74,13 @@ $(document).ready(function() {
     });
 
     $("#submit-form").submit(function(event) {
-    	alert("Handler for .submit() called.");
+    	var link = $("#link-input").val();
 
     	$.ajax({
-    	  url: "getter.php",
+    	  url: "http://th.chisaz.com/backend/setter.php",
     	  type: "get",
     	  data: {
-    	  	link: $("#link-input").value()
+    	  	link: link
     	  },
     	  dataType: "text",
     	  success: function(data) {
