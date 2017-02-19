bc1d4c31f6ecde27daa423f7c53b44e2d7365121
Ryan Hoque <ryanhoque2@gmail.com>
2017-02-18 19:25:27 -0800

    fixed more bugs and getter works fosho w/ hitcount

--- backend/getter.php
+++ backend/getter.php
@@ -8,16 +8,21 @@
 	$animal = $_GET["animal"];
 	$bg = $_GET["bg"];
 
-	$query = "SELECT link FROM thdb WHERE color = '{$color}' AND animal = '{$animal}' AND bg = '{$bg}' ORDER BY -id LIMIT 1";
+	$query = "SELECT * FROM thdb WHERE color = '{$color}' AND animal = '{$animal}' AND bg = '{$bg}' ORDER BY -id LIMIT 1";
 	$response = @mysqli_query($dbc, $query);
 
 	$currtime = time();
 
 	if ($response) {
 		while ($row = mysqli_fetch_array($response)) {
-			if ($currtime > $row['expire-time']) {
+			if ($currtime > strtotime($row['expire-time'])) {
 				die("No link for this combo!");
 			}
+			// update hit count
+			$newHitCount = $row['hitcount'] + 1;
+			$id = $row['id'];
+			$query2 = "UPDATE thdb SET hitcount={$newHitCount} WHERE id={$id}";
+			$response2 = @mysqli_query($dbc, $query2);
 			print json_encode(array("link"=>$row['link']));
 		}
 	} else {
